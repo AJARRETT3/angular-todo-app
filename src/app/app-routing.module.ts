d416a6debea9bed3422e3bd87e8f03fd0b0f4541
Austin Jarrett <austin.jarrett32@gmail.com>
2022-10-13 13:42:27 -0400

    use route guard to protect routes from unauth user

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,15 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { ErrorComponent } from './error/error.component';
 import { ListTodosComponent } from './list-todos-component/list-todos-component.component';
 import { LogoutComponent } from './logout/logout.component';
+import { RouteGuardService } from './service/route-guard.service';
 
 //welcome
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'welcome/:name', component: WelcomeComponent },
-  { path: 'todos', component: ListTodosComponent },
-  { path: 'logout', component: LogoutComponent },
+  { path: 'welcome/:name', component: WelcomeComponent, canActivate: [RouteGuardService] },
+  { path: 'todos', component: ListTodosComponent, canActivate: [RouteGuardService] },
+  { path: 'logout', component: LogoutComponent, canActivate: [RouteGuardService] },
   // ** is LAST
   { path: '**', component: ErrorComponent }
 ];
