5c8e58d645b20a81947cc17dd3892c513609e1e7
Austin Jarrett <austin.jarrett32@gmail.com>
2022-10-17 15:38:18 -0400

    connect backend -- basics

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HelloWorldBean, WelcomeDataService } from '../service/data/welcome-data.service';
 
 @Component({
   selector: 'app-welcome',
@@ -8,13 +9,31 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class WelcomeComponent implements OnInit {
 
-  message: string = 'Some Welcome Message'
-  name: string = ''
+  message: string = 'Some Welcome Message';
+  welcomeMessageFromService: string;
+  name: string;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private service: WelcomeDataService) { }
 
   ngOnInit(): void {
     this.name = this.route.snapshot.params['name']
   }
 
+  getWelcomeMessage() {
+    console.log(this.service.executeHelloWorldBeanService());
+    this.service.executeHelloWorldBeanService().subscribe(
+      response => this.handleSuccessfulResponse(response),
+      error => this.handleErrorResponse(error)
+    );
+  }
+
+  handleSuccessfulResponse(response) {
+    this.welcomeMessageFromService = response.message;
+  }
+
+  handleErrorResponse(error) {
+    this.welcomeMessageFromService = error.error.message
+  }
 }
